c76cbd1d6a142d5960e3c422ac85346eb093370f
USERSAD\jcararau <jorgearturo11.cardenasaraujo@nttdata.com>
2024-02-12 09:07:55 +0100

    quitar comentarios

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,27 +1,15 @@
 import { useState } from "react";
-
 export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, SetinputValue] = useState("");
-
   const onInputChange = (event) => {
     SetinputValue(event.target.value);
   };
-
   const onSubmit = (event) => {
     event.preventDefault();
-    ///setCategories([...categories,'Valorant']);
-    //if (inputValue.trim().length <= 1) return;
-    //setCategories((categories) => [inputValue, ...categories]);
-    //console.log(inputValue);
-    //SetinputValue("");
-
     if (inputValue.trim().length <= 1) return;
     onNewCategory(inputValue.trim());
     SetinputValue("");
   };
-
-  //onChange={ (event) => onInputChange(event)}
-  //<form onSubmit={(event) => onSubmit(event)}>
   return (
     <form onSubmit={onSubmit}>
       <input
